31aca54f7d1dc10e1279f924216bc176f91f0db7
Thomas Casady <tscasady@gmail.com>
2023-01-08 11:16:03 -0600

    test: Add total_calories test for dish.

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,9 +5,27 @@ RSpec.describe Dish, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
   end
+
   describe "relationships" do
     it {should belong_to :chef}
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
+  end
+
+  describe '#total_calories' do
+    
+    let!(:chef_1) { Chef.create!(name: "Tim") }
+    let!(:dish_1) { Dish.create!(name: "Fish", description: "tastes good", chef: chef_1) }
+    let!(:dish_2) { Dish.create!(name: "Veggies", description: "very fresh", chef: chef_1) }
+    let!(:ingredient_1) { dish_1.ingredients.create!(name: 'salmon', calories: 300) }
+    let!(:ingredient_2) { dish_1.ingredients.create!(name: 'butter', calories: 200) }
+    let!(:ingredient_3) { dish_2.ingredients.create!(name: 'arugula', calories: 100) }
+    let!(:ingredient_4) { Ingredient.create!(name: 'salt', calories: 0) }
+    let!(:dish_ing_1) { DishIngredient.create!(dish: dish_1, ingredient: ingredient_4) }
+    let!(:dish_ing_2) { DishIngredient.create!(dish: dish_2, ingredient: ingredient_4) }
+
+    it 'returns the total calories in a dish' do
+      expect(dish_1.total_calories).to eq 2 
+    end
   end
 end
